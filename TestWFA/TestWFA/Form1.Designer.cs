9e098af6e175714f97a6fe8d543705a9c5554753
Andy <andrew.weekes@mymail.nwtc.edu>
2019-02-04 19:17:23 -0600

    changed text again

--- TestWFA/TestWFA/Form1.Designer.cs
+++ TestWFA/TestWFA/Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label1.Location = new System.Drawing.Point(109, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.Size = new System.Drawing.Size(131, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "This text is changed";
+            this.label1.Text = "This text is changed again";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
